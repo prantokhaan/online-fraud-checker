cf5ecf8622e7c0f1ab2dae567029a85b039227c8
Ismatul Islam Pranto <prantokhaan@gmail.com>
2024-06-21 19:17:10 +0600

    complete

--- Subscribe/subscribe.php
+++ Subscribe/subscribe.php
@@ -1,5 +1,5 @@
 <?php
-include '../database/db.php'; // Include your database connection file
+include '../database/db.php';
 
 // Fetch all pricing plans from the database
 $query = "SELECT * FROM pricing";
